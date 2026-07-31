530236a3903fb728718089db365046b8cff4ebc7
ankit <ankit113912@gmail.com>
2026-07-31 06:02:27 -0700

    new feature

--- Prompts_in_langchain/prompt_ui.py
+++ Prompts_in_langchain/prompt_ui.py
@@ -10,7 +10,10 @@ llm = HuggingFaceEndpoint(
 )
 
 st.header("Research Tool")
-paper_input  = st.selectbox
+paper_input  = st.selectbox ("selest Research Paper Name ",["selest...","Attention is All you Need" , "BERT: Pro-traning of deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANS on Image Synthesis"])
+style_input  = st.selectbox ( " Select Explanation Style",[ "Beinner- Friendly", "Technical", "Code-orinted","Mathematical"])
+lenght_input = st.selestbox ( "Select Explanation Lenght ", ["Short (1-2 Paragraphs)", "Medium (3-5 paragraph)","Long (detailed explanation)"])
+
 
 
 if st.button("Summarize"):
