4990be4d62e11d8e5e6b3fe54ce91ed4b043ecea
ankit <ankit113912@gmail.com>
2026-08-02 05:49:08 -0700

    SOlve chathistory problem in a chatbot

--- chatbot.py
+++ chatbot.py
@@ -5,12 +5,19 @@ load_dotenv ()
 
 model = ChatOpenAI()
 
+chat_history = []
+
 while True :
     user_input  = input ('you: ')
+    chat_history.append(user_input)
     if user_input == 'exit':
         break 
-    result = model.invoke (user_input)
+    result = model.invoke (chat_history)
+    chat_history.append(result.content)
     print ("AI: ", result.content)
-    
+
+print (chat_history)
+
+
 
 
