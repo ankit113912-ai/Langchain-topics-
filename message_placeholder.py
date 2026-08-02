508fb84c5baa382065b2d1706e68d1e2c736af37
ankit <ankit113912@gmail.com>
2026-08-02 07:20:04 -0700

    message_placeholder.py

---
+++ message_placeholder.py
@@ -0,0 +1,30 @@
+from langchain_core.prompts import ChaatpromptTemplate, ChatPromptTemplate, MessagesPlaceholder 
+from langchain_core.messages import HumanMessage
+from sympy import content 
+
+
+# chat template 
+chat_template = ChatPromptTemplate.from_messages([
+    ("system","you are a helpful customer support agent "),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human','{query}')
+
+
+])
+
+chat_history = []
+
+# load chat history 
+with open ('chat_history.txt') as f:
+    chat_history.extend(f.readlines())
+
+print (chat_history)
+
+
+# create prompt
+
+chat_template.invoke({'chat_history': chat_history, 'query': HumanMessage(content='where is my refund?')})
+
+print (Prompt)
+
+
