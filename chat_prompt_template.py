412940f8ef1545040bf28c544c4f51887ddbcbee
ankit <ankit113912@gmail.com>
2026-08-02 06:45:21 -0700

    Second way to send a message

--- chat_prompt_template.py
+++ chat_prompt_template.py
@@ -3,8 +3,9 @@ from langchain_core.messages import SystemMessage, HumanMessage
 
 chat_prompt = ChatPromptTemplate  ([
 
-    SystemMessage(content='you are a helpful{domain} expert'),
-    HumanMessage(content='Explain in simple terms, what is {topic}')
+       ('system', 'you are a helpful{domain} expert'),
+
+    ( 'human ', 'Explain in simple terms, what is {topic}?')
   ])
 
 prompt = chat_prompt.invoke({'domain': 'cricket', 'topic': 'Dusra'})
